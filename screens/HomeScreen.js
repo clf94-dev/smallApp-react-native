6574c3ec1a2c4bf9ab8ee415ba65654cc7bb251a
Carmen Lucas <carmenlucas@iMac-de-Carmen.local>
2020-09-27 13:12:34 +0200

    added two more screens with navigation react

---
+++ screens/HomeScreen.js
@@ -0,0 +1,15 @@
+
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+
+export default function HomeScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title='Go To Text' onPress={() => navigation.navigate('Text')}/>
+      <Button title='Go To List' onPress={() => navigation.navigate('List')}/>
+    </View>
+  );
+}
+
+
+const styles=StyleSheet.create
